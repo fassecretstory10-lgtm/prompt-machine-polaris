61bbceaf7e83485be9c8b571bcca385713e5576f
Leonardo Paiva <fas.secretstory10@gmail.com>
2026-03-18 18:53:44 +0000

    feat: connect react app to index.html

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './Prompt_rescue_engine_v1.jsx'
+import './index.css' // Caso você tenha um arquivo de CSS
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
