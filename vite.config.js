788f1efdfa99712b9ce4c83ead6ac11f858f964d
Leonardo Paiva <fas.secretstory10@gmail.com>
2026-03-18 19:04:25 +0000

    fix: total reset to tailwind v4 native vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: true,
-  },
-  server: {
-    port: 3000
-  }
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
