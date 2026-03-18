b8bdef81dc1a8741be59ae2682f9da9e352f0f15
Leonardo Paiva <fas.secretstory10@gmail.com>
2026-03-18 18:59:23 +0000

    fix: complete build configuration with postcss and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.jsx",
+    "./Prompt_rescue_engine_v1.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
